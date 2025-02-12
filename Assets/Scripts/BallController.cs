6fd0343a43b3feaad796b86565de076ceeadd671
Jaxson Kahl <jaxsonkahl@gmail.com>
2025-02-12 12:36:47 -0800

    Created Pin Collection Prefabs

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -3,6 +3,7 @@ using UnityEngine.Events;
 
 public class BallController : MonoBehaviour
 {
+    private bool isBallLaunched;
     [SerializeField] private float force = 1f;
     [SerializeField] private InputManager inputManager;
 
@@ -21,6 +22,8 @@ public class BallController : MonoBehaviour
 
     private void LaunchBall()
     {
+        if (isBallLaunched) return;
+        isBallLaunched = true;
         ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
     }
 }
